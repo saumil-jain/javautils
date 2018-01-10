5e61d11b749fbb3e50b361c19c5dd7794ad419a8
Saumil Jain <saumil-jain@users.noreply.github.com>
2018-01-10 23:29:06 +0530

    Add method in FileIO class to copy file contents from one file to another using character stream.

--- javautils/src/main/java/com/saumil/io/FileIO.java
+++ javautils/src/main/java/com/saumil/io/FileIO.java
@@ -7,7 +7,7 @@ public class FileIO {
     /**
      * Method to copy a files contents into another file by reading the bytes one by one.
      *
-     * @param inputFile The {@link File} to copy from.
+     * @param inputFile  The {@link File} to copy from.
      * @param outputFile The {@link File} to copy to.
      * @throws IOException
      */
@@ -19,6 +19,26 @@ public class FileIO {
             int byteRead;
             while ((byteRead = inputStream.read()) != -1) {
                 outputStream.write(byteRead);
+            }
+        }
+
+    }
+
+    /**
+     * Method to copy file contents into another file by reading the characters one by one.
+     *
+     * @param inputFile  The {@link File} to copy from.
+     * @param outputFile The {@link File} to copy to.
+     * @throws IOException
+     */
+    public static void copyFileUsingCharStream(File inputFile, File outputFile) throws IOException {
+
+        try (Reader reader = new FileReader(inputFile);
+             Writer writer = new FileWriter(outputFile)) {
+
+            int charRead;
+            while ((charRead = reader.read()) != -1) {
+                writer.write(charRead);
             }
         }
 
